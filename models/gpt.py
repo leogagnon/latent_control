6fee0abdb5ed0cd13e1ba9f6b86bb8548f3f0123
Leo Gagnon <leogagnon@gmail.com>
2024-10-23 15:52:32 -0400

    pyvene sandbox notebook

--- models/gpt.py
+++ models/gpt.py
@@ -350,21 +350,21 @@ class GPT(nn.Module):
         return mfu
 
     @torch.no_grad()
-    def generate(self, idx, max_new_tokens, temperature=1.0, top_k=None):
+    def generate(self, input_ids, max_length, temperature=1.0, top_k=None):
         """
-        Take a conditioning sequence of indices idx (LongTensor of shape (b,t)) and complete
+        Take a conditioning sequence of indices input_ids (LongTensor of shape (b,t)) and complete
         the sequence max_new_tokens times, feeding the predictions back into the model each time.
         Most likely you'll want to make sure to be in model.eval() mode of operation for this.
         """
-        for _ in range(max_new_tokens):
+        for _ in range(max_length):
             # if the sequence context is growing too long we must crop it at block_size
             idx_cond = (
-                idx
-                if idx.size(1) <= self.config.block_size
-                else idx[:, -self.config.block_size :]
+                input_ids
+                if input_ids.size(1) <= self.config.block_size
+                else input_ids[:, -self.config.block_size :]
             )
             # forward the model to get the logits for the index in the sequence
-            _, logits = self(idx_cond)
+            logits = self(idx_cond)
             # pluck the logits at the final step and scale by desired temperature
             logits = logits[:, -1, :] / temperature
             # optionally crop the logits to only the top k options
@@ -376,6 +376,6 @@ class GPT(nn.Module):
             # sample from the distribution
             idx_next = torch.multinomial(probs, num_samples=1)
             # append sampled index to the running sequence and continue
-            idx = torch.cat((idx, idx_next), dim=1)
+            input_ids = torch.cat((input_ids, idx_next), dim=1)
 
-        return idx
+        return input_ids
